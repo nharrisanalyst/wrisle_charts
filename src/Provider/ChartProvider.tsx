9f2de8b6aedde1aa3668bd0b12c846dcf8abe13e
Nathan Harris <nharrisanalyst@gmail.com>
2025-05-05 16:30:04 -0700

    feat(chartprovider): added ChartProvider
    add Chart PRovider this includeds creating context and adding all types and props for ChartProvider

---
+++ src/Provider/ChartProvider.tsx
@@ -0,0 +1,87 @@
+import { createContext, useMemo } from "react";
+import { scaleLinear, ScaleLinear  } from 'd3-scale';
+import {max} from 'd3-array';
+import { chartMargin } from "../Constents/ChartConstents";
+
+type MarginType ={
+ left:number;
+ top:number;
+ bottom:number;
+ right:number;
+}
+
+const defaultMargin = {
+    left:25,
+    top:25,
+    bottom:25,
+    right:25
+}
+
+type ChartProviderProps<T> ={
+    data:T[];
+    xAccesor:(d:T) =>number |string| Date,
+    yAccesor:(d:T) => number | string |Date,
+    height:number;
+    width:number;
+    children: any;
+    scaleType:'linear';
+    margin:MarginType;
+}
+
+type ChartContextType<T> = {
+    data:T[];
+    xAccesor:(d:T) =>number |string| Date,
+    yAccesor:(d:T) => number | string |Date,
+    height:number;
+    width:number;
+    scaleType:'linear';
+    margin:MarginType;
+    xScale:ScaleLinear<number,number>;
+    yScale:ScaleLinear<number,number>;
+   
+}
+
+export const  ChartContext = createContext<ChartContextType<any> | null>(null);
+
+const ChartProvider = <T,>({
+                    data, 
+                    height=300, 
+                    width=500, 
+                    xAccesor, 
+                    yAccesor, 
+                    scaleType='linear',
+                    margin = defaultMargin,   
+                    children}: ChartProviderProps<T> ) =>{
+
+    const xScale: ScaleLinear<number,number>  = useMemo(()=> scaleLinear()
+    //@ts-ignore
+                                            .domain([0, max(data, (d)=> xAccesor(d))])
+                                            .range([margin.left, width-margin.right]), 
+                                            [data, xAccesor])
+
+    const yScale :ScaleLinear<number,number>  = useMemo(()=> scaleLinear()
+    //@ts-ignore
+                                            .domain([0, max(data, (d)=> yAccesor(d))])
+                                            .range([height-margin.top, margin.bottom]), 
+                                            [data, yAccesor])
+
+
+    const context: ChartContextType<T> ={
+        data,
+        height,
+        width,
+        yAccesor,
+        xAccesor,
+        xScale,
+        yScale
+    };
+
+    return(
+        <ChartContext.Provider  value={context} >
+        {children}
+        </ChartContext.Provider>
+    );
+}
+
+export default ChartProvider;
+
